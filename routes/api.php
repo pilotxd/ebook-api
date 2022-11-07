af060f4e47648195b67dcba10f3b9eefd81f5dfa
pilotxd <rajendraveo@gmail.com>
2022-11-07 10:50:03 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
 
 /*
@@ -20,14 +21,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('me', [AuthController::class, 'me']);
+Route::get('me', [AuthController::class, 'me']);
 
-//Route::get('/authors', [AuthorController::class, 'index']);
-//Route::get('/authors/{id}', [AuthorController::class, 'show']);
-//Route::post('/authors', [AuthorController::class, 'store']);
-//Route::put('/authors/{id}', [AuthorController::class, 'update']);
-//Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+//API route for register new user
+Route::post('/register', [AuthController::class, 'register']);
+//API route for login user
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::resource('/authors', AuthorController::class)->except(
-    ['create', 'edit']
-);
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('books', BookController::class)->except(
+        ['create', 'edit']
+    );
+
+    Route::resource('authors', AuthorController::class)->except(
+        ['create', 'edit']
+    );
+
+    // API route for logout user
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
